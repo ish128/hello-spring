5fb4ab098949e9042e1c1d7e97e80dcfda1e95c0
rpcopopqpq@gmail.com <rpcopopqpq@gmail.com>
2021-08-13 17:18:10 +0900

    add spring mvc

--- src/main/java/ish128/hellospring/controller/MemberController.java
+++ src/main/java/ish128/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package ish128.hellospring.controller;
 
+import ish128.hellospring.domain.Member;
 import ish128.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,5 +18,25 @@ public class MemberController {
   @Autowired
   public MemberController(MemberService memberService) {
     this.memberService = memberService;
+  }
+
+  @GetMapping("/members/new")
+  public String createForm(){
+    return "/members/createMemberForm.html";
+  }
+
+  @PostMapping("/members/new")
+  public String create(MemberForm form){
+    Member member = new Member();
+    member.setName(form.getName());
+    memberService.join(member);
+    return "redirect:/";
+  }
+
+  @GetMapping("/members")
+  public String list(Model model){
+    List<Member> list = memberService.findMembers();
+    model.addAttribute("members", list);
+    return "/members/memberList.html";
   }
 }
